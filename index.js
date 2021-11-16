a6921cd35bff3149cd408cfe9959b6df0149c367
hvkhvk <vkasikaralar@gmail.com>
2021-11-16 17:16:30 +0300

    Req 4 and Req 5 completed.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-React = React && React.hasOwnProperty('default') ? React['default'] : React;
-ReactDOM = ReactDOM && ReactDOM.hasOwnProperty('default') ? ReactDOM['default'] : ReactDOM;
-
 const width = window.innerWidth;
 const height = window.innerHeight;
 const centerX = width / 2;
@@ -108,53 +105,171 @@ const fourthInnerArc = d3.arc()
     .startAngle(Math.PI * 3 / 2)
     .endAngle(2 * Math.PI);
 
-const Radar = () => (
-    React.createElement( 'svg', { width: width, height: height },
-        React.createElement( 'g', { transform: `translate(${centerX},${centerY})` },
-            React.createElement( 'path', { d: firstOuterArc() }),
-            React.createElement( 'path', { d: firstMiddleOutArc() }),
-            React.createElement( 'path', { d: firstMiddleInArc() }),
-            React.createElement( 'path', { d: firstInnerArc() }),
-            React.createElement( 'line', {x2:outerRadarLine , y2:0, stroke:"black"}),
-            React.createElement( 'line', {x2:0 , y2:-outerRadarLine, stroke:"black"}),
-            React.createElement( 'text', {x:outerRadarLine-50 , y:-outerRadarLine+50, stroke:"black"},"Response"),
-        ),
-        React.createElement( 'g', { transform: `translate(${centerX},${centerY})` },
-            React.createElement( 'path', { d: secondOuterArc() }),
-            React.createElement( 'path', { d: secondMiddleOutArc() }),
-            React.createElement( 'path', { d: secondMiddleInArc() }),
-            React.createElement( 'path', { d: secondInnerArc() }),
-            React.createElement( 'line', {x2:outerRadarLine , y2:0, stroke:"black"}),
-            React.createElement( 'line', {x2:0 , y2:outerRadarLine, stroke:"black"}),
-            React.createElement( 'text', {x:outerRadarLine-50 , y:outerRadarLine-50, stroke:"black"},"Recovery"),
-        ),
-        React.createElement( 'g', { transform: `translate(${centerX},${centerY})` },
-            React.createElement( 'path', { d: thirdOuterArc() }),
-            React.createElement( 'path', { d: thirdMiddleOutArc() }),
-            React.createElement( 'path', { d: thirdMiddleInArc() }),
-            React.createElement( 'path', { d: thirdInnerArc() }),
-            React.createElement( 'line', {x2:0 , y2:outerRadarLine, stroke:"black"}),
-            React.createElement( 'line', {x2:-outerRadarLine , y2:0, stroke:"black"}),
-            React.createElement( 'text', {x:-outerRadarLine , y:outerRadarLine-50, stroke:"black"},"Resilience"),
-        ),
-        React.createElement( 'g', { transform: `translate(${centerX},${centerY})` },
-            React.createElement( 'path', { d: fourthOuterArc() }),
-            React.createElement( 'path', { d: fourthMiddleOutArc() }),
-            React.createElement( 'path', { d: fourthMiddleInArc() }),
-            React.createElement( 'path', { d: fourthInnerArc() }),
-            React.createElement( 'line', {x2:-outerRadarLine , y2:0, stroke:"black"}),
-            React.createElement( 'line', {x2:0 , y2:-outerRadarLine, stroke:"black"}),
-            React.createElement( 'text', {x:-outerRadarLine , y:-outerRadarLine+50, stroke:"black"},"Preparedness"),
-        ),
-        React.createElement( 'g', { transform: `translate(${centerX},${centerY})` },
-            React.createElement( 'text', {x:10, stroke:"black", fontSize:"12px"},"Production"),
-            React.createElement( 'text', {x:innerRadarLine+10, stroke:"black", fontSize:"12px"},"Validation"),
-            React.createElement( 'text', {x:middleInRadarLine+10, stroke:"black", fontSize:"12px"},"Prototype"),
-            React.createElement( 'text', {x:middleOutRadarLine+10, stroke:"black", fontSize:"12px"},"Idea"),
-        ),
-        React.createElement( 'g', { transform: `translate(${centerX},0)` },
-        React.createElement( 'text', {y:40, stroke:"black", fontSize:"2em", textAnchor:"middle"},title),)
-    )
-);
 
-ReactDOM.render(React.createElement( Radar, null ), document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            Var1: "Quadrant",
+            Var2: "Level of implementation",
+            Var3: "Technology",
+        };
+    }
+    componentDidMount() {
+        d3.csv("https://raw.githubusercontent.com/HVKHVK/Digital-Technology-Radar-Project/master/data/Techradar_dataset%20-%20Test%20Assignment.csv", (err, data) => {
+            this.setState({ data: data });
+        });
+    }
+
+    updatePoints(){
+        let allData = this.state.data.map((d) => {
+            let xPoint = 0;
+            let yPoint = 0;
+            let color = "black"
+            if (d[this.state.Var1] === "response" && d[this.state.Var2] === "production") {
+                xPoint = Math.floor(Math.random() * (innerRadarLine-30));
+                yPoint = -Math.sqrt(Math.pow(innerRadarLine - 30, 2) - Math.pow(xPoint, 2));
+            }else if (d[this.state.Var1] === "response" && d[this.state.Var2] === "validation") {
+                xPoint = Math.floor(Math.random() * (middleInRadarLine-30));
+                yPoint = Math.floor(-Math.sqrt(Math.pow(middleInRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "response" && d[this.state.Var2] === "prototype") {
+                xPoint = Math.floor(Math.random() * (middleOutRadarLine-30));
+                yPoint = Math.floor(-Math.sqrt(Math.pow(middleOutRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "response" && d[this.state.Var2] === "idea") {
+                xPoint = Math.floor(Math.random() * (outerRadarLine-30));
+                yPoint = Math.floor(-Math.sqrt(Math.pow(outerRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "recovery" && d[this.state.Var2] === "production") {
+                xPoint = Math.floor(Math.random() * (innerRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(innerRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "recovery" && d[this.state.Var2] === "validation") {
+                xPoint = Math.floor(Math.random() * (middleInRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(middleInRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "recovery" && d[this.state.Var2] === "prototype") {
+                xPoint = Math.floor(Math.random() * (middleOutRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(middleOutRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "recovery" && d[this.state.Var2] === "idea") {
+                xPoint = Math.floor(Math.random() * (outerRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(outerRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "resilience" && d[this.state.Var2] === "production") {
+                xPoint = -Math.floor(Math.random() * (innerRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(innerRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "resilience" && d[this.state.Var2] === "validation") {
+                xPoint = -Math.floor(Math.random() * (middleInRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(middleInRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "resilience" && d[this.state.Var2] === "prototype") {
+                xPoint = -Math.floor(Math.random() * (middleOutRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(middleOutRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "resilience" && d[this.state.Var2] === "idea") {
+                xPoint = -Math.floor(Math.random() * (outerRadarLine-30));
+                yPoint = Math.floor(Math.sqrt(Math.pow(outerRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "preparedness" && d[this.state.Var2] === "production") {
+                xPoint = -Math.floor(Math.random() * (innerRadarLine-30));
+                yPoint = -Math.floor(Math.sqrt(Math.pow(innerRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "preparedness" && d[this.state.Var2] === "validation") {
+                xPoint = -Math.floor(Math.random() * (middleInRadarLine-30));
+                yPoint = -Math.floor(Math.sqrt(Math.pow(middleInRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "preparedness" && d[this.state.Var2] === "prototype") {
+                xPoint = -Math.floor(Math.random() * (middleOutRadarLine-30));
+                yPoint = -Math.floor(Math.sqrt(Math.pow(middleOutRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }else if (d[this.state.Var1] === "preparedness" && d[this.state.Var2] === "idea") {
+                xPoint = -Math.floor(Math.random() * (outerRadarLine-30));
+                yPoint = -Math.floor(Math.sqrt(Math.pow(outerRadarLine - 30, 2) - Math.pow(xPoint, 2)));
+            }
+            if(d[this.state.Var3] === "Natural Language Processing"){
+                color = "red";
+            } else if(d[this.state.Var3] === "Machine Learning"){
+                color = "blue";
+            } else if(d[this.state.Var3] === "Drones"){
+                color = "green";
+            } else if(d[this.state.Var3] === "Blockchain"){
+                color = "orange";
+            } else if(d[this.state.Var3] === "Artificial Intelligence"){
+                color = "yellow";
+            } else if(d[this.state.Var3] === "Social Media"){
+                color = "purple";
+            }
+
+            return {
+                x: xPoint,
+                y: yPoint,
+                color: color
+            };
+        });
+
+        let circles = d3.select(this.chartArea).selectAll('circle').data(allData);
+
+        circles.enter().append('circle')
+            .attr('r', (d) => 3)
+            .attr('fill', (d) => d.color)
+            .attr('cx', (d) => d.x)
+            .attr('cy', (d) => d.y);
+    }
+
+    render() {
+        this.updatePoints()
+
+        return (
+            <div className="container">
+                <div>
+                    <svg width={width} height={height}>
+                        <g transform={`translate(${centerX},${centerY})`}>
+                            <path d={firstOuterArc()}/>
+                            <path d={firstMiddleOutArc()}/>
+                            <path d={firstMiddleInArc()}/>
+                            <path d={firstInnerArc()}/>
+                            <line x2={outerRadarLine} y2={0} stroke={"black"}/>
+                            <line x2={0} y2={-outerRadarLine} stroke={"black"}/>
+                            <text x={outerRadarLine-50} y={-outerRadarLine+50} stroke={"black"}>Response</text>
+                        </g>
+                        <g transform={`translate(${centerX},${centerY})`}>
+                            <path d={secondOuterArc()}/>
+                            <path d={secondMiddleOutArc()}/>
+                            <path d={secondMiddleInArc()}/>
+                            <path d={secondInnerArc()}/>
+                            <line x2={outerRadarLine} y2={0} stroke={"black"}/>
+                            <line x2={0} y2={outerRadarLine} stroke={"black"}/>
+                            <text x={outerRadarLine-50} y={outerRadarLine-50} stroke={"black"}>Recovery</text>
+                        </g>
+                        <g transform={`translate(${centerX},${centerY})`}>
+                            <path d={thirdOuterArc()}/>
+                            <path d={thirdMiddleOutArc()}/>
+                            <path d={thirdMiddleInArc()}/>
+                            <path d={thirdInnerArc()}/>
+                            <line x2={0} y2={outerRadarLine} stroke={"black"}/>
+                            <line x2={-outerRadarLine} y2={0} stroke={"black"}/>
+                            <text x={-outerRadarLine} y={outerRadarLine-50} stroke={"black"}>Resilience</text>
+                        </g>
+                        <g transform={`translate(${centerX},${centerY})`}>
+                            <path d={fourthOuterArc()}/>
+                            <path d={fourthMiddleOutArc()}/>
+                            <path d={fourthMiddleInArc()}/>
+                            <path d={fourthInnerArc()}/>
+                            <line x2={-outerRadarLine} y2={0} stroke={"black"}/>
+                            <line x2={0} y2={-outerRadarLine} stroke={"black"}/>
+                            <text x={-outerRadarLine} y={-outerRadarLine+50} stroke={"black"}>Preparedness</text>
+                        </g>
+                        <g transform={`translate(${centerX},${centerY})`}>
+                            <text x={10} stroke={"black"} fontSize={"12px"}>Production</text>
+                            <text x={innerRadarLine+10} stroke={"black"} fontSize={"12px"}>Validation</text>
+                            <text x={middleInRadarLine+10} stroke={"black"} fontSize={"12px"}>Prototype</text>
+                            <text x={middleOutRadarLine+10} stroke={"black"} fontSize={"12px"}>Idea</text>
+                        </g>
+                        <g transform={`translate(${centerX},0)`}>
+                            <text y={40} stroke={"black"} fontSize={"2em"} textAnchor={"middle"}>{title}</text>
+                        </g>
+                        <g ref={(node) => { this.chartArea = node; }}
+                           transform={`translate(${centerX}, ${centerY})`} />
+                    </svg>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+// Render application
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
